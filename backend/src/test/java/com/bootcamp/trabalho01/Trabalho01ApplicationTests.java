a71c55641ec90ec11f2f5dfce4f0b5831d482217
Marcelo Braga <b3darcademy@outlook.com>
2021-01-30 01:30:26 -0300

    Client project

---
+++ backend/src/test/java/com/bootcamp/trabalho01/Trabalho01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.trabalho01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
